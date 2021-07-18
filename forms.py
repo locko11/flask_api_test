f4643d007ea5f48b6dc76117c403ef1fd17a5dd0
Alex Kotl <lockorocko11@gmail.co>
2021-07-18 12:39:15 +0300

    Chenge styles and add display validation.

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, NumberRange
 from valid import BetterDecimalField
 
 
 class TestForm(FlaskForm):
-    amount = BetterDecimalField('Amount', validators=[DataRequired()], places=2, round_always=True)
+    amount = BetterDecimalField('Amount', validators=[DataRequired(), NumberRange(max=9999999.99)], places=2, round_always=True)
     payway = SelectField('Payway', validators=[DataRequired()],
                          choices=[('USD', 'USD'), ('RUB', 'RUB'), ('EUR', 'EUR')])
     description = TextAreaField('Description')
